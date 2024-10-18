114df0c89921d69c163f0611ba6c440ec65086c1
HenriHTTP <zegordoplays123@gmail.com>
2024-10-18 01:13:08 -0300

    feat: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/components/*.{js,ts,jsx,tsx,html,ejs}',
+    './views/*.{js,ts,jsx,tsx,html,ejs}',
+  ],
+  theme: {},
+  plugins: [],
+};
