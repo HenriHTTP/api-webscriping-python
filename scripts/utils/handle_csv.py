ad954e31a1d870e2ca5fba7b054afefb5993afea
HenriHTTP <zegordoplays123@gmail.com>
2024-10-19 22:06:08 -0300

    refactor: add csv extension and csv file name

--- scripts/utils/handle_csv.py
+++ scripts/utils/handle_csv.py
@@ -9,16 +9,17 @@ import os
 
 
 def generate_csv(data: list[dict], file_name: str) -> bool:
+    csv_file_name: str = f"{file_name}.csv"
     if not data:
         print("The data list is empty. No CSV file will be generated.")
         return False
     try:
         data_frame = pd.DataFrame(data)
-        data_frame.to_csv(file_name, index=False)
-        if os.path.isfile(file_name):
-            print(f"the file '{file_name}' was created successfully.")
+        data_frame.to_csv(csv_file_name, index=False)
+        if os.path.isfile(csv_file_name):
+            print(f"the file '{csv_file_name}' was created successfully.")
         else:
-            print(f"the file '{file_name}' was not found after creation.")
+            print(f"the file '{csv_file_name}.csv' was not found after creation.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
         return False
