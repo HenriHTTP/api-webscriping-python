7049574e1a5c0444fb60ba435b38984be303fa81
HenriHTTP <zegordoplays123@gmail.com>
2024-11-03 22:40:35 -0300

    refactor: add csv file exists

--- scripts/utils/handle_csv.py
+++ scripts/utils/handle_csv.py
@@ -16,12 +16,13 @@ def generate_csv(data: list[dict], file_name: str) -> bool:
     try:
         data_frame = pd.DataFrame(data)
         data_frame.to_csv(csv_file_name, index=False)
-        if os.path.isfile(csv_file_name):
+        csv_file_exists: bool = os.path.isfile(csv_file_name)
+        if csv_file_exists:
             print(f"the file '{csv_file_name}' was created successfully.")
         else:
             print(f"the file '{csv_file_name}.csv' was not found after creation.")
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+    except Exception as error:
+        print(f"Ocorreu um erro: {error}")
         return False
 
     return True
